9284d7aa96cfc53c260802d9ad660610ce8a6185
Antique Dernnell <a.dernnell@gmail.com>
2026-08-16 13:44:44 +0200

    v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,7 @@
-export * from './vec2/vec2-core';
-export * from './vec2/vec2-props';
-export * from './vec2/vec2-binary-ops';
-export * from './vec2/vec2-unary-ops';
-export * from './vec2/vec2-affine';
-
-export * from './vec3/vec3-core';
-export * from './vec3/vec3-props';
-export * from './vec3/vec3-binary-ops';
-export * from './vec3/vec3-unary-ops';
-export * from './vec3/vec3-affine';
-
-export * from './vec4/vec4-core';
-export * from './vec4/vec4-props';
-export * from './vec4/vec4-binary-ops';
-export * from './vec4/vec4-unary-ops';
-export * from './vec4/vec4-affine';
-
-export * from './mat2/mat2-core';
-export * from './mat2/mat2-props';
-export * from './mat2/mat2-binary-ops';
-export * from './mat2/mat2-unary-ops';
-export * from './mat2/mat2-affine';
-export * from './mat2/mat2-decompose';
-
-export * from './mat3/mat3-core';
-export * from './mat3/mat3-props';
-export * from './mat3/mat3-binary-ops';
-export * from './mat3/mat3-unary-ops';
-export * from './mat3/mat3-affine';
-export * from './mat3/mat3-decompose';
-
-export * from './mat4/mat4-core';
-export * from './mat4/mat4-props';
-export * from './mat4/mat4-binary-ops';
-export * from './mat4/mat4-unary-ops';
-export * from './mat4/mat4-affine';
-export * from './mat4/mat4-decompose';
+export * from './vec2';
+export * from './vec3';
+export * from './vec4';
+export * from './mat2';
+export * from './mat3';
+export * from './mat4';
+export * from './quat';
